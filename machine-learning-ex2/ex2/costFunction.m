55b44ac39dd9a93cd4d984ca75c9db520e9a1681
Daniela Mazza <danielamazza@gmail.com>
2017-07-22 10:34:38 +0200

    implemented logistic regression

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,8 +19,11 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-
-
+h = sigmoid(X*theta);
+J = -(1/m)*sum(y.*log(h) + (1-y).*log(1 - h));
+for i = 1:size(grad)
+    grad(i) = (1/m)*sum((h-y).*X(:,i));
+end
 
 
 
